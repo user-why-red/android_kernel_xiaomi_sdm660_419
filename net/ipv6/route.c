99b2113418f85c51a579cc64b6a00d820447a0ce
Eric Dumazet <edumazet@google.com>
2018-10-05 09:17:50 -0700

    UPSTREAM: ipv6: do not leave garbage in rt->fib6_metrics
    In case ip_fib_metrics_init() returns an error, we better
    rewrite rt->fib6_metrics with &dst_default_metrics so that
    we do not crash later in ip_fib_metrics_put()
    
    Fixes: 767a2217533f ("net: common metrics init helper for FIB entries")
    Change-Id: I19174cd7b12d414fbafcce39269e8fbdf0976fac
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: syzbot <syzkaller@googlegroups.com>
    Reviewed-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -3141,6 +3141,8 @@ static struct fib6_info *ip6_route_info_create(struct fib6_config *cfg,
 					       extack);
 	if (IS_ERR(rt->fib6_metrics)) {
 		err = PTR_ERR(rt->fib6_metrics);
+		/* Do not leave garbage there. */
+		rt->fib6_metrics = (struct dst_metrics *)&dst_default_metrics;
 		goto out;
 	}
 
