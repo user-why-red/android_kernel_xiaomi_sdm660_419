800f7c158e7c52c0177851b13407f2d956192174
Shakeel Butt <shakeelb@google.com>
2018-10-26 15:07:41 -0700

    UPSTREAM: memcg: remove memcg_kmem_skip_account
    The flag memcg_kmem_skip_account was added during the era of opt-out kmem
    accounting.  There is no need for such flag in the opt-in world as there
    aren't any __GFP_ACCOUNT allocations within memcg_create_cache_enqueue().
    
    Link: http://lkml.kernel.org/r/20180919004501.178023-1-shakeelb@google.com
    Change-Id: Ia5564b5a82cc8c425219007f72e70bc3e2f02a48
    Signed-off-by: Shakeel Butt <shakeelb@google.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Greg Thelen <gthelen@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1012,9 +1012,6 @@ struct task_struct {
 #endif
 #ifdef CONFIG_MEMCG
 	unsigned			in_user_fault:1;
-#ifdef CONFIG_MEMCG_KMEM
-	unsigned			memcg_kmem_skip_account:1;
-#endif
 #endif
 #ifdef CONFIG_LRU_GEN
 	/* whether the LRU algorithm may apply to this access */
