6c8652c881ccf862847980c12b954c94995e8bd3
Andrew Murray <andrew.murray@arm.com>
2019-08-28 18:50:07 +0100

    UPSTREAM: arm64: atomics: avoid out-of-line ll/sc atomics
    When building for LSE atomics (CONFIG_ARM64_LSE_ATOMICS), if the hardware
    or toolchain doesn't support it the existing code will fallback to ll/sc
    atomics. It achieves this by branching from inline assembly to a function
    that is built with special compile flags. Further this results in the
    clobbering of registers even when the fallback isn't used increasing
    register pressure.
    
    Improve this by providing inline implementations of both LSE and
    ll/sc and use a static key to select between them, which allows for the
    compiler to generate better atomics code. Put the LL/SC fallback atomics
    in their own subsection to improve icache performance.
    
    Change-Id: I7ff9c0bf63f43f5382829e4ef568b6f27996295e
    Signed-off-by: Andrew Murray <andrew.murray@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- arch/arm64/include/asm/atomic.h
+++ arch/arm64/include/asm/atomic.h
@@ -52,16 +52,7 @@
 #define REFCOUNT_PRE_CHECK_ZERO(reg)	"ccmp " #reg ", wzr, #8, pl\n"
 #define REFCOUNT_PRE_CHECK_NONE(reg)
 
-#define __ARM64_IN_ATOMIC_IMPL
-
-#if defined(CONFIG_ARM64_LSE_ATOMICS) && defined(CONFIG_AS_LSE)
-#include <asm/atomic_lse.h>
-#else
-#include <asm/atomic_ll_sc.h>
-#endif
-
-#undef __ARM64_IN_ATOMIC_IMPL
-
+#include <asm/atomic_arch.h>
 #include <asm/cmpxchg.h>
 
 #define ATOMIC_INIT(i)	{ (i) }
