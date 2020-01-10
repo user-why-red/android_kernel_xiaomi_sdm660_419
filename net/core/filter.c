cd0cc59ecd9b3eca1c3a829148011c1ee35f7b82
Lorenz Bauer <lmb@cloudflare.com>
2020-01-10 13:23:36 +0000

    UPSTREAM: net: bpf: Don't leak time wait and request sockets
    commit 2e012c74823629d9db27963c79caa3f5b2010746 upstream.
    
    It's possible to leak time wait and request sockets via the following
    BPF pseudo code:
     
      sk = bpf_skc_lookup_tcp(...)
      if (sk)
        bpf_sk_release(sk)
    
    If sk->sk_state is TCP_NEW_SYN_RECV or TCP_TIME_WAIT the refcount taken
    by bpf_skc_lookup_tcp is not undone by bpf_sk_release. This is because
    sk_flags is re-used for other data in both kinds of sockets. The check
    
      !sock_flag(sk, SOCK_RCU_FREE)
    
    therefore returns a bogus result. Check that sk_flags is valid by calling
    sk_fullsock. Skip checking SOCK_RCU_FREE if we already know that sk is
    not a full socket.
    
    Fixes: edbf8c01de5a ("bpf: add skc_lookup_tcp helper")
    Fixes: f7355a6c0497 ("bpf: Check sk_fullsock() before returning from bpf_sk_lookup()")
    Change-Id: I1c87c4f79d83f42de028f3d6a7f2343469e2e96f
    Signed-off-by: Lorenz Bauer <lmb@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200110132336.26099-1-lmb@cloudflare.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- net/core/filter.c
+++ net/core/filter.c
@@ -5331,8 +5331,7 @@ __bpf_sk_lookup(struct sk_buff *skb, struct bpf_sock_tuple *tuple, u32 len,
 	if (sk) {
 		sk = sk_to_full_sk(sk);
 		if (!sk_fullsock(sk)) {
-			if (!sock_flag(sk, SOCK_RCU_FREE))
-				sock_gen_put(sk);
+			sock_gen_put(sk);
 			return NULL;
 		}
 	}
@@ -5369,8 +5368,7 @@ bpf_sk_lookup(struct sk_buff *skb, struct bpf_sock_tuple *tuple, u32 len,
 	if (sk) {
 		sk = sk_to_full_sk(sk);
 		if (!sk_fullsock(sk)) {
-			if (!sock_flag(sk, SOCK_RCU_FREE))
-				sock_gen_put(sk);
+			sock_gen_put(sk);
 			return NULL;
 		}
 	}
@@ -5437,7 +5435,8 @@ static const struct bpf_func_proto bpf_sk_lookup_udp_proto = {
 
 BPF_CALL_1(bpf_sk_release, struct sock *, sk)
 {
-	if (!sock_flag(sk, SOCK_RCU_FREE))
+	/* Only full sockets have sk->sk_flags. */
+	if (!sk_fullsock(sk) || !sock_flag(sk, SOCK_RCU_FREE))
 		sock_gen_put(sk);
 	return 0;
 }
