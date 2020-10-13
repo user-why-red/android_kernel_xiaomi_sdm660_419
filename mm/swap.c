a489999eead1ff68a4efbfaf1c56bf32bf9e4674
Ralph Campbell <rcampbell@nvidia.com>
2020-10-13 16:56:00 -0700

    UPSTREAM: mm: move call to compound_head() in release_pages()
    The function is_huge_zero_page() doesn't call compound_head() to make sure
    the page pointer is a head page. The call to is_huge_zero_page() in
    release_pages() is made before compound_head() is called so the test would
    fail if release_pages() was called with a tail page of the huge_zero_page
    and put_page_testzero() would be called releasing the page.
    This is unlikely to be happening in normal use or we would be seeing all
    sorts of process data corruption when accessing a THP zero page.
    
    Looking at other places where is_huge_zero_page() is called, all seem to
    only pass a head page so I think the right solution is to move the call
    to compound_head() in release_pages() to a point before calling
    is_huge_zero_page().
    
    Change-Id: I7d6e95ca47458baa9dcc31be2a7692c4b77bd8a1
    Signed-off-by: Ralph Campbell <rcampbell@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Yu Zhao <yuzhao@google.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Link: https://lkml.kernel.org/r/20200917173938.16420-1-rcampbell@nvidia.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- mm/swap.c
+++ mm/swap.c
@@ -819,6 +819,7 @@ void release_pages(struct page **pages, int nr)
 			locked_pgdat = NULL;
 		}
 
+		page = compound_head(page);
 		if (is_huge_zero_page(page))
 			continue;
 
@@ -838,7 +839,6 @@ void release_pages(struct page **pages, int nr)
 				continue;
 		}
 
-		page = compound_head(page);
 		if (!put_page_testzero(page))
 			continue;
 
