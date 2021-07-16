cc8b774280ac9364366602f7e63c90b2ca4b90b9
Ian Kent <raven@themaw.net>
2021-07-16 17:28:18 +0800

    UPSTREAM: kernfs: add a revision to identify directory node changes
    Add a revision counter to kernfs directory nodes so it can be used
    to detect if a directory node has changed during negative dentry
    revalidation.
    
    There's an assumption that sizeof(unsigned long) <= sizeof(pointer)
    on all architectures and as far as I know that assumption holds.
    
    So adding a revision counter to the struct kernfs_elem_dir variant of
    the kernfs_node type union won't increase the size of the kernfs_node
    struct. This is because struct kernfs_elem_dir is at least
    sizeof(pointer) smaller than the largest union variant. It's tempting
    to make the revision counter a u64 but that would increase the size of
    kernfs_node on archs where sizeof(pointer) is smaller than the revision
    counter.
    
    Reviewed-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Ian Kent <raven@themaw.net>
    Link: https://lore.kernel.org/r/162642769895.63632.8356662784964509867.stgit@web.messagingengine.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit 895adbec302e92086359e6fd92611ac3be6d92c3)
    
    Bug: 320903885
    Bug: 219424218
    Bug: 206126556
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Iace824482ea2cca5aff924bf33e9c9ad6a67f27b
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- fs/kernfs/dir.c
+++ fs/kernfs/dir.c
@@ -379,6 +379,7 @@ static int kernfs_link_sibling(struct kernfs_node *kn)
 	/* successfully added, account subdir number */
 	if (kernfs_type(kn) == KERNFS_DIR)
 		kn->parent->dir.subdirs++;
+	kernfs_inc_rev(kn->parent);
 
 	return 0;
 }
@@ -401,6 +402,7 @@ static bool kernfs_unlink_sibling(struct kernfs_node *kn)
 
 	if (kernfs_type(kn) == KERNFS_DIR)
 		kn->parent->dir.subdirs--;
+	kernfs_inc_rev(kn->parent);
 
 	rb_erase(&kn->rb, &kn->parent->dir.children);
 	RB_CLEAR_NODE(&kn->rb);
