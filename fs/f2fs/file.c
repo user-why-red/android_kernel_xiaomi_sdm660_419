8c294c059e5c65ba5848e4a2e933c58fa7649524
Chao Yu <chao@kernel.org>
2023-07-12 04:08:05 +0800

    f2fs: allow f2fs_ioc_{,de}compress_file to be interrupted
    This patch allows f2fs_ioc_{,de}compress_file() to be interrupted, so that,
    userspace won't be blocked when manual {,de}compression on large file is
    interrupted by signal.
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Santhosh <san8754513543@gmail.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -4222,6 +4222,12 @@ static int f2fs_ioc_decompress_file(struct file *filp)
 
 		count -= len;
 		page_idx += len;
+
+		cond_resched();
+		if (fatal_signal_pending(current)) {
+			ret = -EINTR;
+			break;
+		}
 	}
 
 	if (!ret)
@@ -4296,6 +4302,12 @@ static int f2fs_ioc_compress_file(struct file *filp)
 
 		count -= len;
 		page_idx += len;
+
+		cond_resched();
+		if (fatal_signal_pending(current)) {
+			ret = -EINTR;
+			break;
+		}
 	}
 
 	if (!ret)
