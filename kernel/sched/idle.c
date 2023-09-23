9ba92d9536f306103ae16e2bdb84e7601881e379
Kazuki H <kazukih0205@gmail.com>
2023-09-23 18:39:13 +0900

    sched/idle: Enter wfi state instead of polling during active migration
    WFI's wakeup latency is low enough, use that instead of polling and
    burning power.
    
    Signed-off-by: Kazuki H <kazukih0205@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Santhosh <san8754513543@gmail.com>

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -252,8 +252,7 @@ static void do_idle(void)
 		 * broadcast device expired for us, we don't want to go deep
 		 * idle as we know that the IPI is going to arrive right away.
 		 */
-		if (cpu_idle_force_poll || tick_check_broadcast_expired() ||
-				is_reserved(smp_processor_id())) {
+		if (cpu_idle_force_poll || tick_check_broadcast_expired()) {
 			tick_nohz_idle_restart_tick();
 			cpu_idle_poll();
 		} else {
