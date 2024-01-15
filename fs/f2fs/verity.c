34ac0e54c1d7e50d2f4ee7b5c22fb427e1df5a27
Santhosh <san8754513543@gmail.com>
2024-01-15 04:36:35 +0000

    Revert "ext4,f2fs: fix readahead of verity data"
    This reverts commit dd2c7c07acb2bccbe06ac8b1e1f74e249b7fe3b1.
    
    Signed-off-by: Santhosh <san8754513543@gmail.com>

--- fs/f2fs/verity.c
+++ fs/f2fs/verity.c
@@ -302,8 +302,6 @@ static struct page *f2fs_read_merkle_tree_page(struct inode *inode,
 
 	page = find_get_page_flags(inode->i_mapping, index, FGP_ACCESSED);
 	if (!page || !PageUptodate(page)) {
-		DEFINE_READAHEAD(ractl, NULL, inode->i_mapping, index);
-
 		if (page)
 			put_page(page);
 		else if (num_ra_pages > 1)
