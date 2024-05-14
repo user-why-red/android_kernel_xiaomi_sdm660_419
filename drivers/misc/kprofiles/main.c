edc1417031327745b3944a974afb206eeede24a4
Santhosh <santhosh.user.why.red@gmail.com>
2024-05-15 01:55:45 +0530

    Revert "drivers/misc: Upstream Kprofiles to dakkshesh07/Kprofiles"
    This reverts commit 7627afc05a8b1116953d4911e4484c9cc2515f81, reversing
    changes made to 255d7b338c9cf9211de51f60c4bf78a4c96a6906.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- drivers/misc/kprofiles/main.c
+++ drivers/misc/kprofiles/main.c
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (C) 2021-2023 Dakkshesh <dakkshesh5@gmail.com>.
+ * Copyright (C) 2021-2022 Dakkshesh <dakkshesh5@gmail.com>.
  */
 
 #include <linux/delay.h>
@@ -19,21 +19,18 @@
 #define KP_EVENT_BLANK MSM_DRM_EVENT_BLANK
 #define KP_BLANK_POWERDOWN MSM_DRM_BLANK_POWERDOWN
 #define KP_BLANK_UNBLANK MSM_DRM_BLANK_UNBLANK
-#define kprofiles_events msm_drm_notifier
 #elif defined(CONFIG_AUTO_KPROFILES_MI_DRM)
 #define KP_EVENT_BLANK MI_DRM_EVENT_BLANK
 #define KP_BLANK_POWERDOWN MI_DRM_BLANK_POWERDOWN
 #define KP_BLANK_UNBLANK MI_DRM_BLANK_UNBLANK
-#define kprofiles_events mi_drm_notifier
 #elif defined(CONFIG_AUTO_KPROFILES_FB)
 #define KP_EVENT_BLANK FB_EVENT_BLANK
 #define KP_BLANK_POWERDOWN FB_BLANK_POWERDOWN
 #define KP_BLANK_UNBLANK FB_BLANK_UNBLANK
-#define kprofiles_events fb_event
 #endif
 
 static unsigned int kp_override_mode;
-static bool kp_override;
+static bool kp_override = false;
 #ifdef CONFIG_AUTO_KPROFILES
 static bool screen_on = true;
 #endif
@@ -47,12 +44,12 @@ module_param(kp_mode, int, 0664);
 DEFINE_MUTEX(kplock);
 
 /*
- * This function can be used to change profile to any given mode
- * for a specific period of time during any in-kernel event,
- * then return to the previously active mode.
- *
- * usage example: kp_set_mode_rollback(3, 55)
- */
+* This function can be used to change profile to any given mode 
+* for a specific period of time during any in-kernel event,
+* then return to the previously active mode.
+*
+* usage example: kp_set_mode_rollback(3, 55)
+*/
 void kp_set_mode_rollback(unsigned int level, unsigned int duration_ms)
 {
 #ifdef CONFIG_AUTO_KPROFILES
@@ -64,7 +61,7 @@ void kp_set_mode_rollback(unsigned int level, unsigned int duration_ms)
 		return;
 
 	if (unlikely(level > 3)) {
-		pr_err("%s: Invalid mode requested, Skipping mode change\n",
+		pr_err("%s: Invalid mode requested, Skipping mode change",
 		       __func__);
 		return;
 	}
@@ -76,14 +73,15 @@ void kp_set_mode_rollback(unsigned int level, unsigned int duration_ms)
 	kp_override = false;
 	mutex_unlock(&kplock);
 }
+
 EXPORT_SYMBOL(kp_set_mode_rollback);
 
 /*
- * This function can be used to change profile to
- * any given mode during any in-kernel event.
- *
- * usage example: kp_set_mode(3)
- */
+* This function can be used to change profile to 
+* any given mode during any in-kernel event.
+*
+* usage example: kp_set_mode(3)
+*/
 void kp_set_mode(unsigned int level)
 {
 #ifdef CONFIG_AUTO_KPROFILES
@@ -92,7 +90,7 @@ void kp_set_mode(unsigned int level)
 #endif
 
 	if (unlikely(level > 3)) {
-		pr_err("%s: Invalid mode requested, Skipping mode change\n",
+		pr_err("%s: Invalid mode requested, Skipping mode change",
 		       __func__);
 		return;
 	}
@@ -100,31 +98,27 @@ void kp_set_mode(unsigned int level)
 	if (auto_kprofiles)
 		kp_mode = level;
 }
+
 EXPORT_SYMBOL(kp_set_mode);
 
 /*
- * This function returns a number from 0 and 3 depending on the profile
- * selected. The former can be used in conditions to disable/enable
- * or tune kernel features according to a profile mode.
- *
- * usage exmaple:
- *
- * switch (kp_active_mode()) {
- * case 1:
- *	things to be done when battery profile is active
- *	break;
- * case 2:
- *	things to be done when balanced profile is active
- * 	break;
- * case 3:
- *	things to be done when performance profile is active
- *	break;
- * default:
- *	things to be done when kprofiles is disabled
- *	break;
- * }
- *
- */
+* This function returns a number from 0 and 3 depending on the profile 
+* selected. The former can be used in conditions to disable/enable 
+* or tune kernel features according to a profile mode.
+*
+* usage exmaple:
+*
+* if (kp_active_mode() == 3) {
+*	  things to be done when performance profile is active
+* } else if (kp_active_mode() == 2) {
+*	  things to be done when balanced profile is active
+* } else if (kp_active_mode() == 1) {
+*	  things to be done when battery profile is active
+* } else {
+*	  things to be done when kprofiles is disabled
+* }
+*
+*/
 int kp_active_mode(void)
 {
 #ifdef CONFIG_AUTO_KPROFILES
@@ -137,19 +131,25 @@ int kp_active_mode(void)
 
 	if (unlikely(kp_mode > 3)) {
 		kp_mode = 0;
-		pr_info("%s: Invalid value passed, falling back to level 0\n",
-			__func__);
+		pr_info("Invalid value passed, falling back to level 0\n");
 	}
 
 	return kp_mode;
 }
+
 EXPORT_SYMBOL(kp_active_mode);
 
 #ifdef CONFIG_AUTO_KPROFILES
 static inline int kp_notifier_callback(struct notifier_block *self,
 				       unsigned long event, void *data)
 {
-	struct kprofiles_events *evdata = data;
+#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
+	struct msm_drm_notifier *evdata = data;
+#elif defined(CONFIG_AUTO_KPROFILES_MI_DRM)
+	struct mi_drm_notifier *evdata = data;
+#elif defined(CONFIG_AUTO_KPROFILES_FB)
+	struct fb_event *evdata = data;
+#endif
 	int *blank;
 
 	if (event != KP_EVENT_BLANK
@@ -226,7 +226,7 @@ static int __init kp_init(void)
 
 	pr_info("Kprofiles " KPROFILES_VERSION
 		" loaded. Visit https://github.com/dakkshesh07/Kprofiles/blob/main/README.md for information.\n");
-	pr_info("Copyright (C) 2021-2023 Dakkshesh <dakkshesh5@gmail.com>.\n");
+	pr_info("Copyright (C) 2021-2022 Dakkshesh <dakkshesh5@gmail.com>.\n");
 
 	return ret;
 }
