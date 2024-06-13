625bab73c64d03025d45e0e9814c369f0d79fd4e
Santhosh <santhosh.user.why.red@gmail.com>
2024-06-13 08:43:26 +0530

    cgroup-v1: Return length if its <= 1 in pidlist_uniq
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -266,7 +266,7 @@ static int pidlist_uniq(pid_t *list, int length)
 	 * we presume the 0th element is unique, so i starts at 1. trivial
 	 * edge cases first; no work needs to be done for either
 	 */
-	if (length == 0 || length == 1)
+	if (length <= 1)
 		return length;
 	/* src and dest walk down the list; dest counts unique elements */
 	for (src = 1; src < length; src++) {
