9305f24683cee944844e374a5fc1a03573e083a9
Santhosh <santhosh.user.why.red@gmail.com>
2024-08-10 21:24:43 +0530

    proc: Dont let the userspace to modify swappiness value
    Userspace sets vm.swappiness to 100% which makes system to aggresively perform swapping of unused or even critical pages to ZRAM during OOM conditions and this situation makes the device to perform worse overtime with high ZRAM usage which uses more CPU resource.
    
    To avoid this, make the vm.swappiness readonly to avoid being modified by userspace.
    
    The vm.swappiness value is 60% by default which is more balanced of swapping between physical RAM and ZRAM.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -323,6 +323,18 @@ extern struct ctl_table firmware_config_table[];
 int sysctl_legacy_va_layout;
 #endif
 
+/*
+* Make vm.swappiness readonly
+*/
+static int vm_swappiness_readonly(struct ctl_table *table, int write,
+                                  void __user *buffer, size_t *lenp,
+                                  loff_t *ppos) {
+	if (write) {
+		return -EPERM;
+	}
+	return proc_dointvec(table, write, buffer, lenp, ppos);
+}
+
 /* The default sysctl tables: */
 
 static struct ctl_table sysctl_base_table[] = {
@@ -1751,7 +1763,7 @@ static struct ctl_table vm_table[] = {
 		.data		= &vm_swappiness,
 		.maxlen		= sizeof(vm_swappiness),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
+		.proc_handler	= vm_swappiness_readonly,
 		.extra1		= &zero,
 		.extra2		= &one_hundred,
 	},
