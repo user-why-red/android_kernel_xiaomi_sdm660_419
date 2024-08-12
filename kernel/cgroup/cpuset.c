e8ad6d3e743c7b6629926b14c001a942bb6c17e6
Santhosh <santhosh.user.why.red@gmail.com>
2024-08-12 20:34:13 +0530

    cpuset: Tune cpu-assits for better performance and efficiency
    Tuned for sdm660/636
    
    Type: 4x Kryo 260 GoldBased on: ARM Cortex-A73 (4-7)
    Type: 4x Kryo 260 SilverBased on: ARM Cortex-A53 (0-3)
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1796,12 +1796,12 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 #ifdef CONFIG_CPUSETS_ASSIST
 	static struct cs_target cs_targets[] = {
 		/* Little-only cpusets go first */
-		{ "foreground",		"0-5" },
-		{ "background",		"0-2" },
+		{ "foreground",		"4-7" },
+		{ "background",		"0-3" },
 		{ "system-background",	"0-3" },
-		{ "restricted",		"0-5" },
-		{ "top-app",		"0-7" },
-		{ "camera-daemon",	"0-3,6-7" },
+		{ "restricted",		"0-3" },
+		{ "top-app",		"4-7" },
+		{ "camera-daemon",	"0-3,5-7" },
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
