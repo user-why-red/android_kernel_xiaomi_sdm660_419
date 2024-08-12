dde6409326dbcef2cf5bc47413938b8559892ef8
Santhosh <santhosh.user.why.red@gmail.com>
2024-08-12 21:27:47 +0530

    cpuset: Dont use task_is_booster helper
    We dont have it, so just do init comparison
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1806,7 +1806,7 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
 
-	if (task_is_booster(current)) {
+	if (!strcmp(current->comm, "init")) {
 		for (i = 0; i < ARRAY_SIZE(cs_targets); i++) {
 			struct cs_target tgt = cs_targets[i];
 
