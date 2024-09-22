6f0b7c2ed751c1398e4f2583409d7a1164bf6301
BensolBiru <89448703+Ariden24@users.noreply.github.com>
2024-09-22 12:35:35 +0700

    Add KSU

--- fs/read_write.c
+++ fs/read_write.c
@@ -434,10 +434,20 @@ ssize_t kernel_read(struct file *file, void *buf, size_t count, loff_t *pos)
 }
 EXPORT_SYMBOL(kernel_read);
 
+#ifdef CONFIG_KSU
+// KernelSU hook
+extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
+			       size_t *count_ptr, loff_t **pos);
+#endif
+
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
 
+	#ifdef CONFIG_KSU
+	ksu_handle_vfs_read(&file, &buf, &count, &pos); // call KSU fork first
+        #endif
+	
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
 	if (!(file->f_mode & FMODE_CAN_READ))
