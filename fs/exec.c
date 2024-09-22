cc5672a3cd1c029401d73459ffcb827868396e67
BensolBiru <89448703+Ariden24@users.noreply.github.com>
2024-09-22 12:41:06 +0700

    Add KSU

--- fs/exec.c
+++ fs/exec.c
@@ -1736,6 +1736,12 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
+// KernelSU hook
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			       void *envp, int *flags);
+#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1749,6 +1755,10 @@ static int __do_execve_file(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+        #ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
+	#endif
+	
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
