7afc3bacf213ee4395e03f2a224abb9e709074ba
Santhosh <santhosh.user.why.red@gmail.com>
2024-09-23 18:07:46 +0530

    Revert "init: adjustments for retrofitted dynamic partitions"
    This reverts commit b75939041034a7fa06abc06868518184766a855a.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- init/initramfs.c
+++ init/initramfs.c
@@ -605,7 +605,6 @@ static void __init clean_rootfs(void)
 
 static int __initdata do_skip_initramfs;
 
-#if 0
 static int __init skip_initramfs_param(char *str)
 {
 	if (*str)
@@ -614,7 +613,6 @@ static int __init skip_initramfs_param(char *str)
 	return 1;
 }
 __setup("skip_initramfs", skip_initramfs_param);
-#endif
 
 #ifdef CONFIG_BLK_DEV_RAM
 static void __init populate_initrd_image(char *err)
