bddc39b5008d308540bc9e871ae446688fa75bd8
Santhosh <santhosh.user.why.red@gmail.com>
2025-07-22 09:43:02 +0530

    time: posix-timers: move cond_resched() inside loop in posix_timer_add()
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/time/posix-timers.c
+++ kernel/time/posix-timers.c
@@ -158,30 +158,28 @@ static struct k_itimer *posix_timer_by_id(timer_t id)
 
 static int posix_timer_add(struct k_itimer *timer)
 {
-	struct signal_struct *sig = current->signal;
-	int first_free_id = sig->posix_timer_id;
-	struct hlist_head *head;
-	int ret = -ENOENT;
+        struct signal_struct *sig = current->signal;
+        int first_free_id = sig->posix_timer_id;
+        struct hlist_head *head;
+        int ret = -ENOENT;
 
-	do {
-		spin_lock(&hash_lock);
-		head = &posix_timers_hashtable[hash(sig, sig->posix_timer_id)];
-		if (!__posix_timers_find(head, sig, sig->posix_timer_id)) {
-			hlist_add_head_rcu(&timer->t_hash, head);
-			ret = sig->posix_timer_id;
-		}
-		if (++sig->posix_timer_id < 0)
-			sig->posix_timer_id = 0;
-		if ((sig->posix_timer_id == first_free_id) && (ret == -ENOENT))
-			/* Loop over all possible ids completed */
-			ret = -EAGAIN;
-		spin_unlock(&hash_lock);
-	} while (ret == -ENOENT);
-	return ret;
-		cond_resched();
-	}
-	/* POSIX return code when no timer ID could be allocated */
-	return -EAGAIN;
+        do {
+                spin_lock(&hash_lock);
+                head = &posix_timers_hashtable[hash(sig, sig->posix_timer_id)];
+                if (!__posix_timers_find(head, sig, sig->posix_timer_id)) {
+                        hlist_add_head_rcu(&timer->t_hash, head);
+                        ret = sig->posix_timer_id;
+                }
+                if (++sig->posix_timer_id < 0)
+                        sig->posix_timer_id = 0;
+                if ((sig->posix_timer_id == first_free_id) && (ret == -ENOENT))
+                        /* Loop over all possible ids completed */
+                        ret = -EAGAIN;
+                spin_unlock(&hash_lock);
+                cond_resched();
+        } while (ret == -ENOENT);
+
+        return ret;
 }
 
 static inline void unlock_timer(struct k_itimer *timr, unsigned long flags)
