c4b30c259c0f632f639423f2aa192f63cfd386fc
jmpfbmx <jmpf.bmx@gmail.com>
2025-07-29 19:39:02 +0200

    techpack: audio: q6voice: Force AVCS CVP version 1 on SDM630
    Change-Id: Ifb455903091209279eac93f332628c7d0b583324
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- techpack/audio/dsp/q6voice.c
+++ techpack/audio/dsp/q6voice.c
@@ -4442,7 +4442,8 @@ static int voice_get_avcs_version_per_service(uint32_t service_id)
 		return -EINVAL;
 	}
 
-	if (of_machine_is_compatible("qcom,sdm636") ||
+	if (of_machine_is_compatible("qcom,sdm630") ||
+	    of_machine_is_compatible("qcom,sdm636") ||
 	    of_machine_is_compatible("qcom,sdm660")) {
 		common.is_avcs_version_queried = true;
 		return CVP_VERSION_1;
