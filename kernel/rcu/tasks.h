f2c147c7bf76a1575c1329a7575b230997519668
Santhosh <santhosh.user.why.red@gmail.com>
2026-01-25 01:04:49 +0530

    Revert "UPSTREAM: rcu-tasks: Fix show_rcu_tasks_trace_gp_kthread buffer overflow"
    This reverts commit 258ebe16a5f90d0cf396505dcaaae8110d97b31e.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/rcu/tasks.h
+++ kernel/rcu/tasks.h
@@ -1240,7 +1240,7 @@ static void show_rcu_tasks_trace_gp_kthread(void)
 {
 	char buf[64];
 
-	snprintf(buf, sizeof(buf), "N%d h:%lu/%lu/%lu", atomic_read(&trc_n_readers_need_end),
+	sprintf(buf, "N%d h:%lu/%lu/%lu", atomic_read(&trc_n_readers_need_end),
 		data_race(n_heavy_reader_ofl_updates),
 		data_race(n_heavy_reader_updates),
 		data_race(n_heavy_reader_attempts));
