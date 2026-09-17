f202532c3d91ac26e623eaf6dd60a0ba06e82cf7
Santhosh <santhosh.user.why.red@gmail.com>
2026-09-17 19:01:08 +0530

    sched: Give PF_PERF_CRITICAL its own bit
    It was 0x02000000, same as PF_UMH. Usermodehelpers then fail
    __set_cpus_allowed_ptr unless the mask is cpu_lp_mask/cpu_perf_mask,
    and a real perf-critical task looks like a helper.
    
    Move PF_PERF_CRITICAL to 0x00004000. PF_UMH stays on the upstream bit.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1748,7 +1748,7 @@ extern struct pid *cad_pid;
 #define PF_RANDOMIZE		0x00400000	/* Randomize virtual address space */
 #define PF_SWAPWRITE		0x00800000	/* Allowed to write to swap */
 #define PF_MEMSTALL		0x01000000	/* Stalled due to lack of memory */
-#define PF_PERF_CRITICAL	0x02000000	/* Thread is performance-critical */
+#define PF_PERF_CRITICAL	0x00004000	/* Thread is performance-critical */
 #define PF_UMH			0x02000000	/* I'm an Usermodehelper process */
 #define PF_NO_SETAFFINITY	0x04000000	/* Userland is not allowed to meddle with cpus_allowed */
 #define PF_MCE_EARLY		0x08000000      /* Early kill for mce process policy */
