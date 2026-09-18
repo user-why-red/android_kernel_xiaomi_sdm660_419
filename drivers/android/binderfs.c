575f1ee426a7d680b607b6331cf6b1c3947cb6c3
Santhosh <santhosh.user.why.red@gmail.com>
2026-09-19 01:35:47 +0530

    binderfs: Export oneway_spam_detection in features/
    A13 libbinder enables BINDER_ENABLE_ONEWAY_SPAM_DETECTION
    only if /dev/binderfs/features/oneway_spam_detection reads
    as 1. The ioctl was already here, the file was not.
    
    Create features/ on every binderfs mount. binderfs_create_dir
    is no longer tied to BINDER_LOGS.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- drivers/android/binderfs.c
+++ drivers/android/binderfs.c
@@ -560,7 +560,6 @@ out:
 	return dentry;
 }
 
-#ifdef CONFIG_ANDROID_BINDER_LOGS
 static struct dentry *binderfs_create_dir(struct dentry *parent,
 					  const char *name)
 {
@@ -596,6 +595,37 @@ out:
 	return dentry;
 }
 
+static struct binder_features {
+	bool oneway_spam_detection;
+} binder_features = {
+	.oneway_spam_detection = true,
+};
+
+static int binder_features_show(struct seq_file *m, void *unused)
+{
+	seq_printf(m, "%d\n", *(bool *)m->private);
+	return 0;
+}
+DEFINE_SHOW_ATTRIBUTE(binder_features);
+
+static int init_binder_features(struct super_block *sb)
+{
+	struct dentry *dentry, *dir;
+
+	dir = binderfs_create_dir(sb->s_root, "features");
+	if (IS_ERR(dir))
+		return PTR_ERR(dir);
+
+	dentry = binderfs_create_file(dir, "oneway_spam_detection",
+				      &binder_features_fops,
+				      &binder_features.oneway_spam_detection);
+	if (IS_ERR(dentry))
+		return PTR_ERR(dentry);
+
+	return 0;
+}
+
+#ifdef CONFIG_ANDROID_BINDER_LOGS
 static int init_binder_logs(struct super_block *sb)
 {
 	struct dentry *binder_logs_root_dir, *dentry, *proc_log_dir;
@@ -742,6 +772,10 @@ static int binderfs_fill_super(struct super_block *sb, void *data, int silent)
 		if (*name == ',')
 			name++;
 	}
+
+	ret = init_binder_features(sb);
+	if (ret)
+		return ret;
 
 	if (info->mount_opts.stats_mode == STATS_GLOBAL)
 		return init_binder_logs(sb);
