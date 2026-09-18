e3fb82a64211d353918ff15be8c97b1227170d13
Santhosh <santhosh.user.why.red@gmail.com>
2026-09-19 00:57:44 +0530

    binder: Don't log expected ioctl errors
    Every BINDER_FREEZE -EAGAIN (app still has in-flight txns)
    printed a pr_info. That is the normal freezer path, not a
    driver failure.
    
    Skip EINTR/EAGAIN, and only log the rest when binder user
    error debug is on.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5660,8 +5660,10 @@ err:
 	if (thread)
 		thread->looper_need_return = false;
 	wait_event_interruptible(binder_user_error_wait, binder_stop_on_user_error < 2);
-	if (ret && ret != -EINTR)
-		pr_info("%d:%d ioctl %x %lx returned %d\n", proc->pid, current->pid, cmd, arg, ret);
+	if (ret && ret != -EINTR && ret != -EAGAIN)
+		binder_debug(BINDER_DEBUG_USER_ERROR,
+			     "%d:%d ioctl %x %lx returned %d\n",
+			     proc->pid, current->pid, cmd, arg, ret);
 err_unlocked:
 	trace_binder_ioctl_done(ret);
 	return ret;
