8892fdb8ac8e5e0c076208864b4304dd5a8dc714
Santhosh <santhosh.user.why.red@gmail.com>
2026-09-20 20:18:45 +0530

    f2fs: Use i_mmap_sem in finish_preallocate_blocks
    invalidate_lock is a later-kernel mapping field. This
    tree already serializes mmap against truncate with
    i_mmap_sem.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -578,12 +578,12 @@ static int finish_preallocate_blocks(struct inode *inode)
 	}
 
 	f2fs_down_write(&F2FS_I(inode)->i_gc_rwsem[WRITE]);
-	down_write(&inode->i_mapping->invalidate_lock);
+	f2fs_down_write(&F2FS_I(inode)->i_mmap_sem);
 
 	truncate_setsize(inode, i_size_read(inode));
 	ret = f2fs_truncate(inode);
 
-	up_write(&inode->i_mapping->invalidate_lock);
+	f2fs_up_write(&F2FS_I(inode)->i_mmap_sem);
 	f2fs_up_write(&F2FS_I(inode)->i_gc_rwsem[WRITE]);
 
 	if (!ret)
