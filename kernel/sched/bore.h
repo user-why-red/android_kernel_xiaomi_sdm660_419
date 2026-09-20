e9e2166cedf25a31e1d8d5643290e0cdf3901375
Santhosh <santhosh.user.why.red@gmail.com>
2026-09-20 11:11:41 +0530

    sched/bore: Compile out sched_bore when CONFIG_SCHED_BORE=n
    The disabled header exported sched_bore as an undefined
    symbol. Fold it to 0 so fair.c links without bore.o.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/sched/bore.h
+++ kernel/sched/bore.h
@@ -22,10 +22,7 @@ static inline u8 bore_score(struct task_struct *p)
 	return p->bore.penalty >> 8;
 }
 #else
-extern u32 sched_bore;
-extern u32 sched_burst_smoothness;
-extern u32 sched_burst_penalty_offset;
-extern u32 sched_burst_penalty_scale;
+#define sched_bore 0
 static inline void sched_init_bore(void) { }
 static inline void reset_task_bore(struct task_struct *p) { }
 static inline void update_curr_bore(struct task_struct *p, u64 delta_exec) { }
